b0f08b97e1f8513e6668d278dc06be195fd2ad9b
Matthew Hayman <mhayman@ucar.edu>
2024-05-11 07:38:01 -0600

    add dict to define loss function definitons and requirements

--- python/SpiralTorch/loss.py
+++ python/SpiralTorch/loss.py
@@ -7,16 +7,29 @@ from typing import List
 pi_const = 3.14159265359
 
 
-# class loss_fnc_base:
-#     def __init__(self,fwd_var_lst:List[str],noise_var_lst:List[str],name_str:str):
-#         self.fwd_var_lst = fwd_var_lst
-#         self.noise_var_lst = noise_var_lst
-#         self.name_str = name_str
-#     def __str__(self):
-#         return self.name_str
+
+
+class loss_fnc_base:
+    def __init__(self,name_str:str):
+        # self.fwd_var_lst = fwd_var_lst
+        # self.noise_var_lst = noise_var_lst
+        self.forward = None
+        self.name_str = None
+        
+        if name_str in noise_model_dct.keys():
+            self.name_str = name_str
+            self.forward = noise_model_dct[self.name_str]['function']
+        else:
+            print("loss_fnc_base in loss.py initialized with distribution "+name_str)
+            print("This distribution does not exist.  Possible options are:")
+            for key in noise_model_dct.keys():
+                print(key,end=", ")
+        
+    def __str__(self):
+        return self.name_str
     
-#     def forward(self,**fwd_model_vars,**noise_model_vars):
-#         return None
+    # def forward(self,**fwd_model_vars,**noise_model_vars):
+    #     return None
 
 
 
@@ -112,7 +125,7 @@ def gaus_mean_fn(
         y_mean_est:torch.tensor=None,
         counts:torch.tensor=None,
         shot_count:torch.tensor=1.0,
-        variance:torch.tensor=None,
+        variance:torch.tensor=1.0,
         channel_mask:torch.tensor=1.0,
         channel_weight:torch.tensor=1.0)->torch.tensor:
     """
@@ -140,3 +153,35 @@ def gaus_mean_fn(
     """ 
 
     return channel_weight*(channel_mask*((y_mean_est*shot_count-counts)**2/(2*variance*shot_count**2))).sum()
+
+
+noise_model_dct = {
+    'poisson':{
+        'forward_model_inputs':['y_mean_est'],
+        'noise_model_inputs':['counts'],
+        'noise_optional':['shot_count'],
+        'fixed_optional':['channel_mask','channel_weight'],
+        'function':pois_loss_fn,
+    },
+    'deadtime':{
+        'forward_model_inputs':['y_mean_est'],
+        'noise_model_inputs':['counts','active_time'],
+        'noise_optional':[],
+        'fixed_optional':['channel_mask','channel_weight'],
+        'function':deadtime_loss_fn,
+    },
+    'gaussian':{
+        'forward_model_inputs':['y_mean_est','y_var_est'],
+        'noise_model_inputs':['counts'],
+        'noise_optional':['shot_count'],
+        'fixed_optional':['channel_mask','channel_weight'],
+        'function':gaus_fn,
+    },
+    'gaussian_mean':{
+        'forward_model_inputs':['y_mean_est'],
+        'noise_model_inputs':['counts'],
+        'noise_optional':['shot_count','variance'],
+        'fixed_optional':['channel_mask','channel_weight'],
+        'function':gaus_mean_fn,
+    },
+}
